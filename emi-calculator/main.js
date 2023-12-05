9b19fd588f1c609dd49414d2e7c5b3ace5a2c30e
shubham dubey <dubeyshubham902@gmail.com>
2023-12-05 01:15:50 -0800

    swp fix

--- emi-calculator/main.js
+++ emi-calculator/main.js
@@ -240,6 +240,26 @@ function convertToWords(n) {
   }
 }
 
+$(document).ready(function(){
+  var accordionOpen = $('.first_depth p'),
+      secondDepth = $('.second_depth');
+  
+  accordionOpen.on('click', function(){
+      var parentFirstDepth = $(this).closest('.first_depth');
+      
+      if (parentFirstDepth.hasClass('on')) {
+          // If the clicked item is already open, close it
+          parentFirstDepth.removeClass('on');
+      } else {
+          // If the clicked item is closed, close any open items and open it
+          accordionOpen.closest('.first_depth').removeClass('on');
+          parentFirstDepth.addClass('on');
+      }
+  });
+});
+
+
+
 
 
 
